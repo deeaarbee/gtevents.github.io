4d227d5733d2a206653333c99dd705b7f2daa38e
Balaji D R <drbalaji97@gmail.com>
2018-11-27 18:43:55 +0530

    Add files via upload

---
+++ precache-manifest.d8edb4b9297b4007c20fdbfc6e7e0342.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "abab60f06ab52fa7846593e6ae0c9a0b",
+    "url": "/static/media/whatsapp.abab60f0.png"
+  },
+  {
+    "revision": "4281b4bd4f89e6d85aff3180b612bc27",
+    "url": "/static/media/gttrans.4281b4bd.png"
+  },
+  {
+    "revision": "3b9b99039cc0a98dd50c3cbfac57ccb2",
+    "url": "/static/media/Lato-Regular.3b9b9903.ttf"
+  },
+  {
+    "revision": "c62aded729bf7146d491275e5019d7fc",
+    "url": "/static/media/Gilroy-Light.c62aded7.ttf"
+  },
+  {
+    "revision": "0f6e082f42f5247d82c3ade9c2a8f928",
+    "url": "/static/media/Gilroy-ExtraBold.0f6e082f.ttf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "97fbb0c56246c261448f",
+    "url": "/static/js/main.97fbb0c5.chunk.js"
+  },
+  {
+    "revision": "cd60255c50f86a62461f",
+    "url": "/static/js/1.cd60255c.chunk.js"
+  },
+  {
+    "revision": "97fbb0c56246c261448f",
+    "url": "/static/css/main.30903e48.chunk.css"
+  },
+  {
+    "revision": "b8d75c34ca1e79d475f39604a7efaef4",
+    "url": "/index.html"
+  }
+];
